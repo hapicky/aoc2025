388ddd83eb0ccfc5c2f8c72a59f6f4dc21bc4ac5
Ryo Suzuki <hapicky@users.noreply.github.com>
2026-02-05 00:01:00 +0900

    Solve day1 part1

--- src/main/scala/day01/Part1.scala
+++ src/main/scala/day01/Part1.scala
@@ -1,7 +1,40 @@
 package day01
 
+import scala.io.Source
+
 object Part1 {
+  case class State(point: Int, password: Int)
+
+  val SIZE = 100
+
+  def rotate(state: State, rotation: String): State = {
+    val direction = rotation.head
+    val distance = rotation.tail.toInt
+    val delta = direction match {
+      case 'L' => -distance
+      case 'R' => distance
+      case _ => sys.error(s"invalid direction: $direction")
+    }
+
+    val nextPoint =
+      Math.floorMod(state.point + delta, SIZE)
+
+    val nextPassword =
+      if (nextPoint == 0) state.password + 1 else state.password
+
+    State(nextPoint, nextPassword)
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Day01 part1")
+    val inputPath = args.headOption.getOrElse(
+      sys.error("usage: 'sbt \"runMain day01.Part1 input/day01/sample.txt\"'")
+    )
+
+    val finalState =
+      Source.fromFile(inputPath)
+        .getLines()
+        .foldLeft(State(50, 0))(rotate)
+
+    println(s"answer: ${finalState.password}")
   }
 }
